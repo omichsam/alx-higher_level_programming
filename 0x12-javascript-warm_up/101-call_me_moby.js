7e86af575fb58416be5944f75313b7a5a7103a8e
omichsam <omichsam@gmail.com>
2023-11-16 08:58:42 +0300

    s

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-  while (x > 0) {
-    theFunction();
-    x--;
-  }
-};
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const process = require('process');
-const message = 'No argument';
-if (process.argv[2] === undefined) {
-  console.log(message);
-} else {
-  console.log(process.argv[2]);
-}
