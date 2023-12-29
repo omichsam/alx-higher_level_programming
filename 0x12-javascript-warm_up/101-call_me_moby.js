1bda2d7785d2d3f43874b8df8b8e03a4c23525fc
omichsam <omichsam@gmail.com>
2023-12-29 12:18:26 +0300

    extra act

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-
-module.exports = {
-  /*
- callMeMoby - Invokes a function x times.
-  */
-
-  callMeMoby: function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
   }
 };
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-
-module.exports = {
-  /*
-Invokes a function after incrementing its argument by 1.
-  */
-
-  addMeMaybe: function (number, theFunction) {
-    number++;
-    theFunction(number);
-  }
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
 };
