410adaf0b1d3c64c816556c1392a1c501de5fc38
omichsam <omichsam@gmail.com>
2024-01-03 23:35:06 +0300

    rewdem file]

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-
+const process = require('process');
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
+// The first argument is the file path
+const file = process.argv[2];
+// The content of the file must be written in utf-8
+fs.readFile(file, 'utf8', function (err, data) {
+  // If an error occurred during the reading, print the error object
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
